63026661568a8a807a33945ac8264dd0f7b5d889
Jan Bańczerowski <jasiuu2605@gmail.com>
2023-03-27 18:15:46 +0200

    adding programatic navigation to filtered events page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,16 +1,20 @@
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/EventList";
 import EventsSearch from "../../components/events/EventsSearch";
+import { useRouter } from "next/router";
+
 const AllEventsPage = () => {
   const events = getAllEvents();
+  const router = useRouter();
 
-  const findEventsHandler = (year, month) => { 
-
-   }
+  const findEventsHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  };
 
   return (
     <>
-      <EventsSearch onSearch={findEventsHandler}/>
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </>
   );
